416a4792ab55477d19560b29be8335dd053ed63c
RomanCox <rom.cox@gmail.com>
2022-03-23 09:48:09 +0300

    Lesson 12 Commit without snapshot

---
+++ src/stories/AppWithRedux.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { AddItemForm } from '../AddItemForm';
+import action from '@storybook/addon-actions'
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import AppWithRedux from '../AppWithRedux';
+import { ReduxStoreProviderDecorator } from './ReduxStoreProviderDecorator';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'TodoList/AppWithRedux',
+  component: AppWithRedux,
+  decorators: [ReduxStoreProviderDecorator]
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+} as ComponentMeta<typeof AppWithRedux>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof AppWithRedux> = (args) => <AppWithRedux />;
+
+export const AppWithReduxStory = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+AppWithReduxStory.args = {};
